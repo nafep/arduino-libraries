cb5e1a83d88826d4c0b1ec75d5dadb6712302ce1
nafep <github@nafep.it>
2016-04-30 02:30:27 +0200

    Add fading capability and "background" updating

--- Led/Led.cpp
+++ Led/Led.cpp
@@ -7,44 +7,160 @@
 
 #include "Led.h"
 
-Led::Led(int8_t pin, boolean initial_state) :
-		_on_off(initial_state),
+Led::Led(int8_t pin) :
 		_pin(pin),
-		_flash_times(3),
-		_flash_wait(333),
-		_intensity(100) {
-	pinMode(_pin, OUTPUT);
-	toggle(_on_off);
+		_intensity(0),
+		_startIntensity(0),
+		_targetIntensity(0),
+		_minIntensity(0),
+		_maxIntensity(100),
+		_fadeDurationMillis(0),
+		_flashTimes(3) {
+	setPin(_pin);
+	_applyIntensityToLed();
+}
+
+Led::Led(int8_t pin, uint8_t maxIntensity) :
+		Led(pin) {
+	setMaxIntensity(maxIntensity);
+}
+
+Led::Led(int8_t pin, uint8_t minIntensity, uint8_t maxIntensity) :
+		Led(pin) {
+	setMinIntensity(minIntensity);
+	setMaxIntensity(maxIntensity);
+}
+
+void Led::setup() {
+}
+
+void Led::loop() {
+	if(_intensity != _targetIntensity) {
+		/**
+		 * Todo: Adapt "t" computation to make _fadeDurationMillis represent the time
+		 *       to fade from minIntensity to maxIntensity and not from startIntensity
+		 *       to targetIntensity. (Or consider making this an optional behavior.)
+		 */
+		float k = 1.0;
+		if(0)
+			k = abs(_targetIntensity-_startIntensity) / (_maxIntensity-_minIntensity);
+
+		float t = k * float(millis()-_startMillis) / _fadeDurationMillis;
+
+		if(t<1.0)
+			_intensity = _startIntensity + int8_t(t*float(_targetIntensity-_startIntensity));
+		else
+			_intensity = _targetIntensity;
+
+		_applyIntensityToLed();
+	}
 }
 
 int8_t Led::pin() {
 	return _pin;
 }
 
-boolean Led::state() {
-	return _on_off;
+boolean Led::isOn() {
+	return(_intensity == _maxIntensity);
 }
 
-uint8_t Led::intensityPercent() {
+boolean Led::isOff() {
+	return(_intensity == _minIntensity);
+}
+
+uint8_t Led::intensity() {
 	return _intensity;
+}
+
+uint8_t Led::minIntensity() {
+	return _minIntensity;
+}
+
+uint8_t Led::maxIntensity() {
+	return _maxIntensity;
+}
+
+uint8_t Led::targetIntensity() {
+	return _targetIntensity;
+}
+
+uint16_t Led::fadeDuration() {
+	return _fadeDurationMillis;
 }
 
 void Led::setPin(int8_t pin) {
 	_pin = pin;
-	pinMode(_pin, OUTPUT);
-	toggle(LOW);
+	setup();
 }
 
-void Led::toggle(boolean onOff) {
-	if (_pin >= 0 && onOff != _on_off) {
-		_on_off = onOff;
-		analogWrite(_pin, _on_off ? map(_intensity, 0, 100, 0.0, 1023.0) : 0);
+void Led::setMinIntensity(uint8_t percent) {
+	if(percent > _maxIntensity)
+		percent = _maxIntensity;
+	_minIntensity = percent;
+
+	if(_intensity < _minIntensity)
+		setIntensity(_minIntensity);
+}
+
+void Led::setMaxIntensity(uint8_t percent) {
+	if(percent < _minIntensity)
+		percent = _minIntensity;
+	if(percent > 100)
+		percent = 100;
+	_maxIntensity = percent;
+
+	if(_intensity > _maxIntensity)
+		setIntensity(_maxIntensity);
+}
+
+void Led::setIntensityImmediately(uint8_t percent) {
+	if(percent < _minIntensity) {
+		percent = _minIntensity;
+	}
+	else {
+		if(percent > _maxIntensity)
+			percent = _maxIntensity;
+	}
+	_intensity = percent;
+	_targetIntensity = _intensity;
+	_applyIntensityToLed();
+}
+
+void Led::setIntensity(uint8_t target) {
+	if(_fadeDurationMillis>0) {
+		_startIntensity = _intensity;
+		_targetIntensity = target;
+		_startMillis = millis();
+	}
+	else {
+		_intensity = target;
+		_applyIntensityToLed();
 	}
 }
 
+void Led::setFadeDurationMillis(uint16_t duration) {
+	_fadeDurationMillis = duration;
+}
+
+void Led::setFlash(uint8_t times) {
+	_flashTimes = times;
+}
+
+void Led::setFlash(uint8_t times, uint16_t cycleMillis) {
+	_flashTimes = times;
+	setFadeDurationMillis(uint16_t(cycleMillis/2));
+}
+
+void Led::toggle(boolean onOff) {
+	if(onOff)
+		setIntensity(_maxIntensity);
+	else
+		setIntensity(_minIntensity);
+}
+
 void Led::toggle() {
-	if (_pin > 0)
-		toggle(!_on_off);
+	if(isOn() || isOff())
+		toggle(!isOn());
 }
 
 void Led::on() {
@@ -56,6 +172,7 @@ void Led::off() {
 }
 
 void Led::flash(uint8_t times, uint16_t wait) {
+/*
 	if (wait == 0) {
 		if(times == 0)
 			wait = _flash_wait;
@@ -69,9 +186,15 @@ void Led::flash(uint8_t times, uint16_t wait) {
 		toggle();
 		delay(uint16_t(wait / 2));
 	}
+*/
 }
 
-void Led::setFlash(uint8_t times, uint16_t wait) {
-	_flash_times = times;
-	_flash_wait = wait;
+/**
+ * This is the method that actually sets the intensity
+ * directly.
+ * Caution. No input value check performed!
+ */
+void Led::_applyIntensityToLed() {
+	if(_pin>=0)
+		analogWrite(_pin,LED_gamma[_intensity]);
 }
--- Led/Led.h
+++ Led/Led.h
@@ -11,37 +11,79 @@
 #include "Arduino.h"
 #include <stdint.h>
 
-#define L_ON HIGH
-#define L_OFF LOW
+#define LED_ON HIGH
+#define LED_OFF LOW
+
+/**
+ *  Inspired from https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+ *  Rescaled to have a gamma intensity corresponding to a **percent** value.
+ */
+const uint8_t LED_gamma[] = { \
+	  0,   0,   0,   0,   0,   0,   0,   0,   0,   0, \
+	  0,   1,   1,   1,   1,   1,   2,   2,   2,   2, \
+	  3,   3,   4,   4,   5,   5,   6,   7,   7,   8, \
+	  9,  10,  11,  11,  13,  13,  15,  16,  17,  18, \
+	 20,  21,  22,  24,  25,  27,  29,  31,  32,  35, \
+	 37,  39,  41,  43,  46,  48,  50,  52,  56,  58, \
+	 61,  64,  67,  70,  73,  77,  79,  83,  86,  90, \
+	 95,  98, 102, 105, 110, 114, 119, 122, 127, 131, \
+	137, 142, 146, 152, 156, 162, 167, 173, 177, 184, \
+	191, 196, 203, 208, 215, 220, 228, 233, 241, 247, \
+	255 };
 
 // ========================================================
 
 class Led {
 public:
-	Led(int8_t pin = -1, boolean initial_state = L_OFF);
-	virtual ~Led() {};
+	Led(int8_t pin = -1);
+	Led(int8_t pin, uint8_t maxIntensity);
+	Led(int8_t pin, uint8_t minIntensity, uint8_t maxIntensity);
+	virtual ~Led() {}
+
+	virtual void setup();
+	virtual void loop();
 
 	int8_t pin();
-	boolean state();
-	uint8_t intensityPercent();
-	void setPin(int8_t pin);
-	virtual void toggle(boolean onOff);
-	void toggle();
-	void on();
-	void off();
-	void flash(uint8_t times = 0, uint16_t wait = 0);
-	void setFlash(uint8_t times = 3, uint16_t wait = 0);
-	void setIntensityPercent(uint8_t percent = 100);
-	void setFlashSoftness(uint16_t slowness = 0);
+	boolean isOn();
+	boolean isOff();
+	uint8_t intensity();
+	uint8_t minIntensity();
+	uint8_t maxIntensity();
+	uint8_t targetIntensity();
+	uint16_t fadeDuration();
 
-protected:
-	boolean _on_off;
-	int8_t _pin;
-	uint8_t _intensity;     // in percent (0 - 100)
+	void setPin(int8_t pin);
+	void setMinIntensity(uint8_t percent);
+	void setMaxIntensity(uint8_t percent);
+	void setFadeDurationMillis(uint16_t duration);
+
+	void setFlash(uint8_t times);
+	void setFlash(uint8_t times, uint16_t cycleMillis);
+
+	void setIntensityImmediately(uint8_t percent);
+	void setIntensity(uint8_t percent);
+
+	void flash(uint8_t times = 0, uint16_t cycleMillis = 0);
+
+	virtual void toggle(boolean onOff);
+	virtual void toggle();
+	virtual void on();
+	virtual void off();
 
 private:
-	uint8_t _flash_times;
-	uint16_t _flash_wait;
+	int8_t _pin;
+
+	uint8_t _intensity;        // Current intensity
+	uint8_t _startIntensity;
+	uint8_t _targetIntensity;
+	uint8_t _minIntensity;     // in percent (0 - 100)
+	uint8_t _maxIntensity;
+	uint32_t _startMillis;
+
+	uint16_t _fadeDurationMillis;
+	uint8_t _flashTimes;
+
+	void _applyIntensityToLed();
 };
 
 #endif /* Led_H */
